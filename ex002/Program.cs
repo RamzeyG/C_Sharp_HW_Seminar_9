d28adc61432f832bc548edaf23c8d621e22e11d9
Ramzey <remzesii@gmail.com>
2022-06-12 08:31:12 +0500

    второе задание

---
+++ ex002/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии
+
+int M = new Random().Next(0, 49);
+int N = new Random().Next(50, 99);
+//int y = (M + N)*(N - M + 1)/2;
+int summ = 0;
+
+Console.Write($"Сумма числе от {M} до {N}");
+
+void Func(){
+    if (M > N){
+        return;
+    }
+    summ = summ + M;
+    M = M + 1;
+    Func();
+}
+
+Func();
+
+
+Console.Write($" равна {summ}");
+//Console.WriteLine($"{y}");

313d28a9a83b55f17f9d85836ab5abdc9d079ece
Ramzey <remzesii@gmail.com>
2022-06-12 08:30:55 +0500

    первое задание

---
+++ ex001/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+int M = new Random().Next(0, 49);
+int N = new Random().Next(50, 99);
+
+Console.WriteLine($"Четные числа в промежутке от {M} до {N}: ");
+
+void Func(){
+    if (M%2 == 0){
+        Console.WriteLine($"{M}");
+    }
+    else{
+        Console.WriteLine($"{M+1}");
+    }
+    M = M + 2;
+    if (M >= N){
+        return;
+    }
+    Func();
+}
+
+
+
+Func();
+
